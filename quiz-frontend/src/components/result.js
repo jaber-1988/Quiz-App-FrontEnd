a0aafedfaae86d10701ee72bfcef9988170588e8
jaber-1988 <jaber1988.1988@gmail.com>
2022-05-18 15:22:11 +0200

    setup

---
+++ quiz-frontend/src/components/result.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const result = () => {
+
+  return (
+    <div>
+        <h2>Your Score:</h2>
+       
+        <input type="text" value="your Name" placeholder="your Name"></input>
+        
+        <button>Save</button>
+    </div>
+  )
+}
