1f7c46e5c1b812c41b6f74aa87dc05f84641d3d5
jaber-1988 <jaber1988.1988@gmail.com>
2022-05-19 14:02:16 +0200

    add result

--- quiz-frontend/src/App.js
+++ quiz-frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/quiz" element={<Quiz />} />
-        <Route path="/" element={<Result />} />
+        <Route path="/result" element={<Result />} />
       </Routes>
     </div>
   );
--- quiz-frontend/src/components/Result.js
+++ quiz-frontend/src/components/Result.js
@@ -1,14 +1,33 @@
 import React from 'react'
+import { useState } from "react";
+import axios from "axios";
 
-export default function Result  () {
 
+const apiHost = process.env.REACT_APP_API_HOST
+
+
+
+export default function Result() {
+
+  const [playerNew, setPlayerNew] = useState({})
+
+  const createPlayer = async () => {
+    try {
+      const respons = await axios.post(`${apiHost}/addPlayer/`, playerNew);
+      console.log(respons);
+    } catch (err) {
+      console.error(err);
+    }
+  }
   return (
     <div>
-        <h2>Your Score:</h2>
-       
-        <input type="text" value="your Name" placeholder="your Name"></input>
-        
-        <button>Save</button>
+      <h2>Your Score:</h2>
+
+      <label>Your Name: </label>
+      <input type="text" required defaultValue="" onChange={(e) => { setPlayerNew({ ...playerNew, yourName: e.target.value }); console.log(playerNew); }} />
+
+
+      <button onClick={() => { createPlayer() }}>Save</button>
     </div>
   )
 }
