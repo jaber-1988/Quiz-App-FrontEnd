c72e8b5709f429867fd0d70b584df750a2acc9d7
Aisha Noori <aisha.noori2000@gmail.com>
2022-06-01 11:49:05 +0200

    added new branch

--- quiz-frontend/src/App.js
+++ quiz-frontend/src/App.js
@@ -3,15 +3,17 @@ import Home from "./components/Home";
 import Quiz from "./components/Quiz";
 import Result from "./components/Result";
 import { Route, Routes } from "react-router";
-/* import { GiTrophyCup } from "react-icons/gi"; */
+import { GiTrophyCup } from "react-icons/gi"; 
 
 function App() {
 
   return (
     <div className="App">
-      <h1>
-        Quiz <img src="images/images.png" alt="adamak" />
-      </h1>
+      <header>
+        <h1>
+          Quiz <GiTrophyCup/> 
+        </h1>
+      </header>
 
       <Routes>
         <Route path="/" element={<Home />} />
